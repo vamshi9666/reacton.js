fc6a2463dc646019b06c2e762a3fa97894f641b3
vamshikn <vamshikn@upscalesoft.in>
2018-06-01 22:58:46 +0530

    able to connect bundle for eletron

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ const {app, BrowserWindow} = require('electron')
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 800, height: 600,
+        webPreferences: {
+          webSecurity: false
+        }
+      })
   
     // and load the index.html of the app.
-    win.loadFile('index.html')
+    win.loadURL(`file://${__dirname}/build/index.html`)
   }
   
   app.on('ready', createWindow)
